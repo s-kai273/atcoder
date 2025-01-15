32189814fde7694a92492b30c30ab0db41f31cf9
skai273 <kinoshita116@gmail.com>
2025-01-15 21:00:26 +0900

    solve beginner contest 373 d

---
+++ beginner_contest/beginner_contest_371/d.py
@@ -0,0 +1,8 @@
+N = int(input())
+X_list = list(map(int, input().split()))
+P_list = list(map(int, input().split()))
+Q = int(input())
+
+for _ in range(Q):
+    L, R = map(int, input().split())
+    
