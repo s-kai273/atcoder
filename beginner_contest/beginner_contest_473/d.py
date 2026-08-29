63fdf78677aaddcb9c6340936c6520c52be0c57e
skai273 <kinoshita116@gmail.com>
2026-08-29 23:24:53 +0900

    solve beginner contest 473 d

--- beginner_contest/beginner_contest_473/d.py
+++ beginner_contest/beginner_contest_473/d.py
@@ -2,17 +2,18 @@ n, k = map(int, input().split())
 answer = list()
 
 
-def dfs(i: int, a_parts: list[str], cur_sum: int):
-    if i > n or cur_sum > k:
+def dfs(i: int, a_parts: list[int], cur_sum: int):
+    if i == n:
+        remain = k - cur_sum
+        if remain % n == 0:
+            answer.append(a_parts.copy() + [remain // n])
         return
+
     for a in range(k + 1):
-        a_parts.append(str(a))
+        a_parts.append(a)
         cur_sum += i * a
         if cur_sum == k:
-            if i == n:
-                answer.append(a_parts.copy())
-            else:
-                answer.append(a_parts.copy() + ["0"] * (n - i))
+            answer.append(a_parts.copy() + [0] * (n - i))
         elif i < n and cur_sum < k:
             dfs(i + 1, a_parts, cur_sum)
         a_parts.pop()
@@ -23,4 +24,5 @@ def dfs(i: int, a_parts: list[str], cur_sum: int):
 
 dfs(1, [], 0)
 for comb in answer:
+    comb = [str(val) for val in comb]
     print(" ".join(comb))
