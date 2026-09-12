b7c21989384d601482ac9d69e0a579e72cf39d77
skai273 <kinoshita116@gmail.com>
2026-09-12 23:45:28 +0900

    solve beginner contest 475 c

--- beginner_contest/beginner_contest_475/c.py
+++ beginner_contest/beginner_contest_475/c.py
@@ -4,30 +4,37 @@
 n, s, l = map(int, input().split())
 a_list = list(map(int, input().split()))
 
-r_dist = 0
-r_city = 1
-for i in range(s - 1, n - 1):
-    if r_dist + a_list[i] > l:
-        break
-    r_dist += a_list[i]
-    r_city += 1
-for i in range(s - 2, -1, -1):
-    if r_dist + a_list[i] * 2 > l:
-        break
-    r_dist += a_list[i] * 2
-    r_city += 1
+left, right = 1, s
+left_dist = sum(a_list[left - 1 : s - 1])
+right_dist = sum(a_list[s - 1 : right - 1])
 
-l_dist = 0
-l_city = 1
-for i in range(s - 2, -1, -1):
-    if l_dist + a_list[i] > l:
-        break
-    l_dist += a_list[i]
-    l_city += 1
-for i in range(s - 1, n - 1):
-    if l_dist + a_list[i] * 2 > l:
-        break
-    l_dist += a_list[i] * 2
-    l_city += 1
+answer = 0
+while left <= s and right <= n:
+    city_count = right - left + 1
+    dist = min(2 * left_dist + right_dist, left_dist + 2 * right_dist)
+    # dist = 2 * left_dist + right_dist
+    if dist <= l and city_count > answer:
+        answer = city_count
+    if dist > l:
+        left += 1
+        left_dist = sum(a_list[left - 1 : s - 1])
+    else:
+        right += 1
+        right_dist = sum(a_list[s - 1 : right - 1])
 
-print(max(r_city, l_city))
+# left, right = 1, s
+# left_dist = sum(a_list[left - 1 : s - 1])
+# right_dist = sum(a_list[s - 1 : right - 1])
+#
+# while left <= s and right <= n:
+#     city_count = right - left + 1
+#     dist = left_dist + 2 * right_dist
+#     if dist <= l and city_count > answer:
+#         answer = city_count
+#     if dist > l:
+#         left += 1
+#         left_dist = sum(a_list[left - 1 : s - 1])
+#     else:
+#         right += 1
+#         right_dist = sum(a_list[s - 1 : right - 1])
+print(answer)
